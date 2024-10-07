b315eb1bba35d2beb1c18e343932d493b6999758
msyphus <mark.syphus@gmail.com>
2024-10-07 16:32:56 -0600

    added skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,12 +1,52 @@
 function Skills() {
 
     return (
-        <div>
+        <div className='contentContainer'>
             <div>
-                Coding skills include:
+                <div>
+                    Coding skills include:
+                </div>
+                <ul>
+                    <li>Batch Script</li>
+                    <li>Bootstrap</li>
+                    <li>CSS</li>
+                    <li>DevExtreme</li>
+                    <li>Docker</li>
+                    <li>Express</li>
+                    <li>Git</li>
+                    <li>HTML</li>
+                    <li>Java</li>
+                    <li>JavaScript</li>
+                    <li>jQuery</li>
+                    <li>Linux</li>
+                    <li>Node</li>
+                    <li>Nginx</li>
+                    <li>R</li>
+                    <li>R Shiny</li>
+                    <li>React</li>
+                    <li>Redux</li>
+                    <li>SQL</li>
+                    <li>VBA</li>
+                    <li>XML</li>
+                </ul>
             </div>
             <div>
-                Non-coding skills include:
+                <div>
+                    Non-coding skills include:
+                </div>
+                <ul>
+                    <li>Agile</li>
+                    <li>Auditing</li>
+                    <li>Lean Six Sigma</li>
+                    <li>Statistical Analysis</li>
+                    <li>Jira</li>
+                    <li>Project Management</li>
+                    <li>Documentation</li>
+                    <li>Quality Management</li>
+                    <li>Process Management</li>
+                    <li>Science</li>
+                    <li>Management</li>
+                </ul>
             </div>
         </div>
     )
