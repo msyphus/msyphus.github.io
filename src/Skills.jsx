f9b9b4d5d718d923f937f4767a9701d284625d21
msyphus <mark.syphus@gmail.com>
2024-10-09 14:53:03 -0600

    reformatted skills page

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,54 +1,21 @@
 function Skills() {
 
     return (
-        <div className='scrollingDiv centered'>
-            <div className='listDiv'>
-                <h3>
-                    Coding skills include:
-                </h3>
-                <ul className='listFormat'>
-                    <li>Batch Script</li>
-                    <li>Bootstrap</li>
-                    <li>CSS</li>
-                    <li>DevExtreme</li>
-                    <li>Docker</li>
-                    <li>Express</li>
-                    <li>Git</li>
-                    <li>HTML</li>
-                    <li>Java</li>
-                    <li>JavaScript</li>
-                    <li>jQuery</li>
-                    <li>Linux</li>
-                    <li>Node</li>
-                    <li>Nginx</li>
-                    <li>R</li>
-                    <li>R Shiny</li>
-                    <li>React</li>
-                    <li>Redux</li>
-                    <li>SQL</li>
-                    <li>VBA</li>
-                    <li>XML</li>
-                </ul>
-            </div>
-            <div className='listDiv'>
-                <h3>
-                    Non-coding skills include:
-                </h3>
-                <ul>
-                    <li>Agile</li>
-                    <li>Auditing</li>
-                    <li>Business Management</li>
-                    <li>Documentation</li>
-                    <li>Lean Six Sigma</li>
-                    <li>Manufacturing</li>
-                    <li>Jira</li>
-                    <li>Process Management</li>
-                    <li>Project Management</li>
-                    <li>Quality Management</li>
-                    <li>Science</li>
-                    <li>Statistical Analysis</li>
-                </ul>
-            </div>
+        <div className='scrollingDiv'>
+            <h4>
+                Coding Skills Include:  
+            </h4> 
+            <p>
+                Batch Script, Bootstrap, CSS, DevExtreme, Docker, Express, Git, HTML, Java, JavaScript,
+                jQuery, Linux, MySQL, Node, Nginx, R, R Shiny, React, Redux, Sequelize, SQL, SSMS (SQL Server Management Studio), VBA, XML 
+            </p>
+            <h4 className='pt-3'>
+                Other Skills Include:  
+            </h4> 
+            <p>
+                Agile (Scrum Master), Auditing, Business Management, Documentation, Lean Six Sigma (Master Black Belt), Manufacturing, Jira,
+                Process Management, Project Management, Quality Management, Science, Statistical Analysis
+            </p>
         </div>
     )
 }
