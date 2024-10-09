ce5bcfc2be748166bcc6b2862e3b95aa54eaa257
msyphus <mark.syphus@gmail.com>
2024-10-09 12:42:12 -0600

    changed contact info presentation

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,38 @@ function App() {
           <Row className='text-center'>
             <h3>Full Stack Web Developer</h3>
           </Row>
+          <Row className='text-center'>
+            <Col className='px-0'>
+              <div>
+                <a href='tel:+13856667626'>
+                  +1 385-666-7626
+                </a>
+              </div>
+              <div>
+                <a href='mailto:mark.syphus@gmail.com'>
+                  mark.syphus@gmail.com
+                </a>
+              </div>
+            </Col>
+          </Row>
+          <Row className='text-center mt-1'>
+            <Col>
+              <a 
+                href='https://www.linkedin.com/in/mark-syphus/' 
+                target='_blank' 
+                title='https://www.linkedin.com/in/mark-syphus/'
+              >
+                <i className="fab fa-linkedin icon" />
+              </a>
+              <a 
+                href='https://github.com/msyphus' 
+                target='_blank' 
+                title='https://github.com/msyphus'
+              >
+                <i className="fab fa-github icon" />
+              </a>
+            </Col>
+          </Row>
         </Col>
         <Col>
           <LanguageToggler
--- src/MyTabs.jsx
+++ src/MyTabs.jsx
@@ -2,7 +2,6 @@ import { Tabs, Tab } from "react-bootstrap";
 import AboutMe from './AboutMe';
 import Projects from './Projects';
 import Skills from './Skills';
-import ContactMe from './ContactMe';
 
 function MyTabs() {
     
@@ -28,12 +27,6 @@ function MyTabs() {
                 title={'Skills'}
             >
                 <Skills />
-            </Tab>
-            <Tab 
-                eventKey={'contact'}
-                title={'Contact Me'}
-            >
-                <ContactMe />
             </Tab>
         </Tabs>
     )
